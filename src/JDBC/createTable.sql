24e2abdde7e2125e60c1229d7ef0efae9b52ab25
yang7.cooper@gmail.com <yang7.cooper@gmail.com@88210c02-5be5-0d03-aabd-dd9b6bc01f17>
2012-02-18 04:16:45 +0000

    schema

---
+++ src/JDBC/createTable.sql
@@ -0,0 +1,54 @@
+--CS5430 System Security Course Project
+--DB Schema
+------------------------------------------------------------------
+--Create Tables:
+------------------------------------------------------------------
+CREATE TABLE Users(
+	username VARCHAR(20),
+	password VARCHAR(20),
+	roleID INT, 
+	name VARCHAR(20),
+	phone VARCHAR(10),
+	location VARCHAR(20), 
+	currProj VARCHAR(50), 
+	PRIMARY KEY (username)
+);
+CREATE TABLE Department(
+	did INT, 
+	dname VARCHAR(20), 
+	dhead VARCHAR(20), 
+	dheadname VARCHAR(20),
+	PRIMARY KEY (did),
+	FOREIGN KEY (dname) REFERENCES Users(username)
+);
+CREATE TABLE WorksFor(
+	worker VARCHAR(20),
+	master VARCHAR(20),
+	did INT,
+	PRIMARY KEY(worker, master),
+	FOREIGN KEY (worker) REFERENCES Users(username),
+	FOREIGN KEY (master) REFERENCES Users(username),
+	FOREIGN KEY (worker) REFERENCES Department(did)
+);
+CREATE TABLE Friends( 
+	friend1 VARCHAR(20),
+	friend2 VARCHAR(20),
+	PRIMARY KEY(worker, master),
+	FOREIGN KEY (friend1) REFERENCES Users(username),
+	FOREIGN KEY (friend2) REFERENCES Users(username)
+);
+CREATE TABLE DeptDiscBoard(
+	deptID INT,
+	msgID INT,
+	msgContent VARCHAR(500),
+	PRIMARY KEY (deptID, msgID), 
+	FOREIGN KEY (deptID) REFERENCES Department(did),
+);
+CREATE TABLE CompanyMsgBoard(
+	msgID INT,
+	msgContent VARCHAR(500),
+	PRIMARY KEY (msgID)
+);
+
+
+
