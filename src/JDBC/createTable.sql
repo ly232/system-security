472e6cf109583bdf93b9b6478833b0b6cb096c42
yang7.cooper@gmail.com <yang7.cooper@gmail.com@88210c02-5be5-0d03-aabd-dd9b6bc01f17>
2012-02-20 16:29:26 +0000

    create table scripts

--- src/JDBC/createTable.sql
+++ src/JDBC/createTable.sql
@@ -1,2 +1,114 @@
-THIS OLD SCHEMA IS NO LONGER USED!!!
-WE DECIDED TO AUTO-GENERATE TABLE SCHEMA IN MYSQLBENCH.
+delimiter $$
+
+CREATE DATABASE `entnetdb_v2` /*!40100 DEFAULT CHARACTER SET utf8 */$$
+
+delimiter $$
+
+CREATE TABLE `currloc` (
+  `loc_id` int(11) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `user_id` (`user_id`),
+  KEY `loc_id` (`loc_id`),
+  CONSTRAINT `loc_id` FOREIGN KEY (`loc_id`) REFERENCES `location` (`loc_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+delimiter $$
+
+CREATE TABLE `department` (
+  `did` int(11) NOT NULL AUTO_INCREMENT,
+  `dname` varchar(20) DEFAULT NULL,
+  `dhead_uid` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`did`),
+  KEY `dhead_uid` (`dhead_uid`),
+  CONSTRAINT `` FOREIGN KEY (`dhead_uid`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+delimiter $$
+
+CREATE TABLE `friend` (
+  `user1` varchar(20) NOT NULL,
+  `user2` varchar(20) NOT NULL,
+  `message` varchar(100) NOT NULL,
+  `msg_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user1`,`user2`),
+  KEY `user1` (`user1`),
+  KEY `user2` (`user2`),
+  CONSTRAINT `user1` FOREIGN KEY (`user1`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `user2` FOREIGN KEY (`user2`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+delimiter $$
+
+CREATE TABLE `location` (
+  `loc_id` int(11) NOT NULL AUTO_INCREMENT,
+  `loc_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`loc_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+delimiter $$
+
+CREATE TABLE `manage` (
+  `manager_id` varchar(20) NOT NULL,
+  `worker_id` varchar(20) NOT NULL,
+  PRIMARY KEY (`worker_id`),
+  KEY `manager_id` (`manager_id`),
+  KEY `worker_id` (`worker_id`),
+  CONSTRAINT `manager_id` FOREIGN KEY (`manager_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `worker_id` FOREIGN KEY (`worker_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+delimiter $$
+
+CREATE TABLE `postworkmessage` (
+  `msg_id` int(11) NOT NULL,
+  `did` int(11) NOT NULL,
+  `msg_content` varchar(100) NOT NULL,
+  PRIMARY KEY (`msg_id`,`did`),
+  KEY `did` (`did`),
+  CONSTRAINT `did` FOREIGN KEY (`did`) REFERENCES `department` (`did`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+delimiter $$
+
+CREATE TABLE `project` (
+  `proj_id` int(11) NOT NULL AUTO_INCREMENT,
+  `proj_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`proj_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+delimiter $$
+
+CREATE TABLE `user` (
+  `user_id` varchar(20) NOT NULL,
+  `user_pwd` varchar(45) DEFAULT NULL,
+  `contact_info` varchar(100) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+delimiter $$
+
+CREATE TABLE `workat` (
+  `userID_workat` varchar(20) NOT NULL,
+  `deptID_workat` int(11) DEFAULT NULL,
+  PRIMARY KEY (`userID_workat`),
+  KEY `userID_workat` (`userID_workat`),
+  KEY `deptID_workat` (`deptID_workat`),
+  CONSTRAINT `deptID_workat` FOREIGN KEY (`deptID_workat`) REFERENCES `department` (`did`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `userID_workat` FOREIGN KEY (`userID_workat`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+delimiter $$
+
+CREATE TABLE `workon` (
+  `uid` varchar(20) NOT NULL,
+  `pid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`uid`),
+  KEY `uid` (`uid`),
+  KEY `pid` (`pid`),
+  CONSTRAINT `pid` FOREIGN KEY (`pid`) REFERENCES `project` (`proj_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `uid` FOREIGN KEY (`uid`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
