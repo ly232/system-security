d0c5f626838be208c08df2e8c363f53de84e601b
crazymooner <crazymooner@88210c02-5be5-0d03-aabd-dd9b6bc01f17>
2012-02-23 21:09:49 +0000

    integrate version

---
+++ trunk/src/entnetclient/Constants.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entnetclient;
+
+/**
+ *
+ * @author Lin
+ */
+public class Constants {
+    public static final int LOGIN_REQUEST_ID = 1;
+    public static final int REGIST_REQUEST_ID = 2;
+    //public static final int REGIST_INFO_ID = 3;
+}
