5cb3e783a0a211272f5d3ffb4a0b9286949a6358
crazymooner <crazymooner@88210c02-5be5-0d03-aabd-dd9b6bc01f17>
2012-03-14 20:55:49 +0000

    Security

---
+++ src/Security/SecurityManager.java
@@ -0,0 +1,14 @@
+package Security;
+
+import sun.security.jca.GetInstance;
+
+public class SecurityManager {
+		private SecurityManager(){
+			
+		}
+		
+		public static   Object GetInstance(String mode){
+				return null;
+		}
+		
+}
